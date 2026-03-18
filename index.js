28841d7f80f58648acb9b6e5bc9b790c0dca9321
Chandra Swaraj <mchandraswaraj@gmail.com>
2026-03-18 20:51:05 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+function rollDice(){  
+    var randomNumber1 = Math.floor(Math.random()*6)+1;
+    document.querySelector("img").setAttribute("src", "dice"+randomNumber1+".png");
+    var randomNumber2 = Math.floor(Math.random()*6)+1;
+    document.getElementsByClassName("img2")[0].setAttribute("src", "dice"+randomNumber2+".png");
+    if(randomNumber1 > randomNumber2){
+        document.querySelector("h1").textContent = "Player 1 wins";
+    }else if(randomNumber1 < randomNumber2){
+        document.querySelector("h1").textContent = "Player 2 wins";
+    }else{
+        document.querySelector("h1").textContent = "Draw";
+    }   
+}
+document.getElementById("roll-btn").addEventListener("click", rollDice);
